313a71b00a0775b2ba557b5ce89c2568b87ec233
martinkr <github@mkrause.info>
2014-03-25 17:24:12 +0100

    demo

--- jquery.tilttoreveal.js
+++ jquery.tilttoreveal.js
@@ -35,7 +35,9 @@
 /**
  * Setup requestAnimationFrame
  */
-window.requestAnimFrame=(function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};})();
+if(!window.requestAnimFrame=) {
+	window.requestAnimFrame=(function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};})();
+}
 
 /**
  * Plugin
